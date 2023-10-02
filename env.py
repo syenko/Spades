dfdfc509dc46db752c34bab0556f805d071940b3
Sabrina <59457991+syenko@users.noreply.github.com>
2023-10-01 17:09:29 -0700

    Create template environment, add requirements.txt

---
+++ env.py
@@ -0,0 +1,15 @@
+import gymnasium as gym
+
+class SpadesEnv(gym.Env):
+
+    def __init__(self):
+        pass
+
+    def step(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def close(self):
+        pass
