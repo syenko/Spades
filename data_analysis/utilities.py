61357ba846b630f3b1a968ea56cb3dc35ab413f6
Sabrina <59457991+syenko@users.noreply.github.com>
2023-11-08 11:33:45 -0800

    Remove testing code

--- data_analysis/utilities.py
+++ data_analysis/utilities.py
@@ -12,8 +12,3 @@ def str_to_cards_list(card_str: str) -> list[Card]:
         Card(Suit(int(card_str[i])), str_value_to_num_value(card_str[i + 1])) for i in range(0,len(card_str) - 2, 2)
     ]
     return cards
-
-test_str = "1J24262Q2A3438050608090T0K1317191T1Q1K27293335373903141522282J2K32363J040J0Q0A1216181A23252T3T3Q3K3A0207"
-
-for card in str_to_cards_list(test_str):
-    print(card)
