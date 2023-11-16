0060f0e822b5baa20eac6e3504cc8346b6dfc486
Sabrina <59457991+syenko@users.noreply.github.com>
2023-11-16 10:29:12 -0800

    Fix card conversion methods

--- data_analysis/utilities.py
+++ data_analysis/utilities.py
@@ -4,11 +4,11 @@ from gameplay.constants import Suit
 
 def str_value_to_num_value(value: str) -> int:
     cards_in_order = "23456789tjqka"
-    return cards_in_order.index(value.lower())
+    return cards_in_order.index(value.lower()) + 2
 
 
 def str_to_cards_list(card_str: str) -> list[Card]:
     cards = [
-        Card(Suit(int(card_str[i])), str_value_to_num_value(card_str[i + 1])) for i in range(0,len(card_str) - 2, 2)
+        Card(Suit(int(card_str[i])), str_value_to_num_value(card_str[i + 1])) for i in range(0, len(card_str), 2)
     ]
     return cards
