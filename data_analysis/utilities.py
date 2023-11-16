c1c216c0c1723d5897452e618f9fe236b688f830
karachou <143539447+karachou@users.noreply.github.com>
2023-11-16 10:31:39 -0800

    Update utilities.py
    Added data processing

--- data_analysis/utilities.py
+++ data_analysis/utilities.py
@@ -2,13 +2,65 @@ from gameplay.card import Card
 from gameplay.constants import Suit
 
 
+
 def str_value_to_num_value(value: str) -> int:
     cards_in_order = "23456789tjqka"
-    return cards_in_order.index(value.lower())
+    return cards_in_order.index(value.lower()) + 2
 
 
 def str_to_cards_list(card_str: str) -> list[Card]:
     cards = [
-        Card(Suit(int(card_str[i])), str_value_to_num_value(card_str[i + 1])) for i in range(0,len(card_str) - 2, 2)
+        Card(Suit(int(card_str[i])), str_value_to_num_value(card_str[i + 1])) for i in range(0, len(card_str), 2)
     ]
     return cards
+
+def processing(filename):
+    file = open(filename)
+
+    lines = [x.strip() for x in file]
+
+    info = lines[0]
+    mode = int(info[5])
+    variation = int(info[17])
+
+    numrounds = int((len(lines) - 2)/17)
+
+    rounds = []
+
+    for i in range(numrounds):
+        print(i)
+        tricks = []
+        for j in range(13):
+            line = lines[4+j+i*17]
+            start = line[0]
+            cardsplayed = str_to_cards_list(line[2:])
+            dict = {
+                "start": start,
+                "cards": cardsplayed
+            }
+            tricks.append(dict)
+
+        hand = str_to_cards_list(lines[2 + i * 17][6:])
+        bid = lines[3 + i * 17]
+        trickstaken = lines[(i+1) * 17][7:]
+        score = lines[(i+1) * 17 + 1][6:]
+
+        dict = {
+            "cards": hand,
+            "bids": bid,
+            "tricks": tricks,
+            "trickstaken": trickstaken,
+            "score": score
+        }
+        rounds.append(dict)
+
+    dict = {
+        "mode": mode,
+        "variation": variation,
+        "rounds": rounds
+    }
+
+    print(dict)
+    return dict
+
+processing("660480.txt")
