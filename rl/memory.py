be6830bace33f57fc7b64cca5c720f1db0c28534
Sabrina <59457991+syenko@users.noreply.github.com>
2024-02-06 10:28:31 -0800

    Implement additional estimator functions, improve memory

--- rl/memory.py
+++ rl/memory.py
@@ -6,6 +6,8 @@ Stores previous states / actions
 import random
 from collections import namedtuple
 
+import numpy as np
+
 Transition = namedtuple('Transition', ['state', 'action', 'reward', 'next_state', 'done'])
 
 class Memory:
@@ -23,8 +25,10 @@ class Memory:
             k = self.batch_size
         sample = random.sample(self.memory, k)
 
-        # TODO: return the sample in some usable form (maybe want to split by stuff)
-        return sample
+        # turns sample into arrays by type (array of all sampled states, all sampled actions, all sampled rewards, etc)
+        by_type = np.array(sample).T.tolist()
+
+        return by_type
 
     def save(self, state, action, reward, next_state, done):
         if len(self.memory) == self.memory_size:
@@ -34,3 +38,16 @@ class Memory:
 
     def size(self):
         return len(self.memory)
+
+# Testing memory
+# mem = Memory(100, 10)
+# for i in range(100):
+#     mem.save(
+#         random.randint(0, 10),
+#         random.randint(10, 20),
+#         random.randint(20, 30),
+#         random.randint(30, 40),
+#         random.randint(40, 50)
+#     )
+#
+# print(mem.sample())
